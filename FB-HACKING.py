e588686ec97cab163b1f90546841530cdb4043b7
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2021-03-05 17:20:56 +0200

    Create FB-HACKING.py

---
+++ FB-HACKING.py
@@ -0,0 +1,45 @@
+import time
+import os
+p = "01283494514"
+l = input("\033[0;31m password ≈) ")
+if l == (p) :
+	print ("\033[0;32m loding ...")
+	time.sleep(3)
+	print ("\033[0;33m ok .")
+else :
+		braeke
+		
+os.system("clear")
+time.sleep(4)
+
+print ("\033[0;31m                   ,.")
+print ("\033[0;32m                 ,   `.")
+print ("\033[0;33m               ,  _<>_ `.")
+print ("\033[0;34m             , .- ____`-.`.")
+print ("\033[0;35m           , _.-      ``-._`.")
+print ("\033[0;36m         , ,       /\      `.`.")
+print ("\033[0;37m       ,  /.._  O /  \ O  _.,\ `.")
+print ("\033[0;38m     , / /  \ ``-;.--.:-   /  \ \`.")
+print ("\033[0;39m   ,  : :    \  /\`., /\  /    : : `.")
+print ("\033[0;31m  < <>| |    LWES ELTASA hack :-.!;.`")
+print ("\033[0;32m   `. : :    /  \/, `.\/  \    ; ; ,") 
+print ("\033[0;33m     `.\ \  /_..-:`-- ;-.._\  / /") 
+print ("\033[0;34m       `. \`    O \  / O   ` / ,") 
+print ("\033[0;35m         `.`._     \/     _, ,") 
+print ("\033[0;36m           `..``-.____.-  ,,") 
+print ("\033[0;37m             `.`-.____.- ,") 
+print ("\033[0;38m               `.  <>  ,") 
+print ("\033[0;39m                 `.  ,")
+print ("\033[0;31m FACEBOOK.GROUPS.HACKING")
+
+m = input("\033[0;33m group id = ")
+n = input("\033[0;34m your profile id = ")
+lwes = ("\033[0;35m https://m.facebook.com/group/add_admin/?group_id=")
+eltasa = ("&user_id=")
+hack = ("&added&_rdrChange")
+time.sleep(3)
+print (lwes,m,eltasa,n,hack)
+
+print ("subscribe here ☟")
+
+print ("\033[0;31m https://youtube.com/channel/UCugYk-edxlAeiB7F66Af2Dg")
